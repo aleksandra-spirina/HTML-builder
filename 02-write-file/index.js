c5586c73e6850d6e36953722febd03e20cb3148b
Aleksandra <aleksandra.spirina.link@gmail.com>
2022-05-25 12:42:55 +0300

    feat: solve 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -8,46 +8,29 @@ const byеText = "Thanks for the chatter. Bye-bye!"
 
 let writer = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
-writer.write('data','UTF8');
-// writer.end();/* w  w  w.j av  a  2s .c o m*/
-
-writer.on('finish',function(){
-  console.log("finished");
+writer.on('finish', function () {
+	console.log(byеText);
 });
-
-writer.on('error',function(err){
-  console.log(err.stack);
-});
-
 
 const inputInterface = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 });
 
-// writer.write("text", 'UTF8');
-// writer.end();
-
-let all_text = '';
+console.log(helloText);
 
 inputInterface.on('line', data => {
-	let text = data.toString();
-	writer.write(data, 'UTF8');
-	// all_text.concat(data);
 
-	if (text === 'exit') {
-		console.log(byеText);
-		console.log(all_text)
+	if (data.toString() === 'exit') {
 		inputInterface.close();
 		writer.end();
-		}
+	} else {
+		writer.write(data, 'UTF8');
+		writer.write('\n', 'UTF8');
+	}
+
 })
 
-// console.log(all_text)
-
-// inputInterface.question(helloText, function (answer) {
-// 	writer.write(answer);
-// 	console.log("Closing the interface");
-// 	inputInterface.close();
-// 	writer.close()
-// });
+inputInterface.on('close', () => {
+	writer.end();
+});
